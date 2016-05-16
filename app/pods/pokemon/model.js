f1cc3bdfa52e992779b2424b6085fed627eb5cd5
Michael Yockey <mike@heysparkbox.com>
2016-05-15 23:14:31 -0400

    fix: Loading images over TLS

--- app/pods/pokemon/model.js
+++ app/pods/pokemon/model.js
@@ -7,7 +7,7 @@ inflector.irregular('pokemon', 'pokemon');
 export default DS.Model.extend({
   name: DS.attr('string'),
   imageUrl: Ember.computed('id', function() {
-    return `http://pokeapi.co/media/sprites/pokemon/${this.get('id')}.png`;
+    return `https://pokeapi.co/media/sprites/pokemon/${this.get('id')}.png`;
   }),
   trainee: DS.belongsTo('trainee'),
 
